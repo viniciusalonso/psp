44d7241525958e9c433c18f03c2694142dcaaad3
Vinícius A <vba321@hotmail.com>
2019-09-12 11:38:53 -0300

    Added validations to Payable

--- models/payable.js
+++ models/payable.js
@@ -1,10 +1,41 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     const Payable = sequelize.define('Payable', {
-        paymentDate: DataTypes.DATE,
-        status: DataTypes.STRING,
-        amount: DataTypes.NUMERIC(6,2),
-        transactionId: DataTypes.INTEGER
+        paymentDate: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            validate: {
+                isDate: true,
+                notEmpty: true,
+                notNull: true
+            },
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                notNull: true
+            },
+        },
+        amount: {
+            type: DataTypes.NUMERIC(6,2),
+            allowNull: false,
+            validate: {
+                isFloat: true,
+                notEmpty: true,
+                notNull: true
+            },
+        },
+        transactionId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                isInt: true,
+                notEmpty: true,
+                notNull: true
+            },
+        }
     }, {});
     Payable.associate = function(models) {
         Payable.belongsTo(models.Transaction, { foreignKey: 'transactionId', as: 'transaction' })
